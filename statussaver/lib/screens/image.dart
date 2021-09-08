b675a3cc19e5ad22c2ad9741863dbe0b7f91031b
flavian-anselmo <anselmoflavian4@gmail.com>
2021-09-09 00:00:46 +0300

    getting device permission to access the device internal storage but no permissions set in the manifest file

--- statussaver/lib/screens/image.dart
+++ statussaver/lib/screens/image.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:statussaver/services/permission.dart';
 
 class ImageScreen extends StatefulWidget {
   ImageScreen({Key? key}) : super(key: key);
@@ -9,9 +11,37 @@ class ImageScreen extends StatefulWidget {
 }
 
 class _ImageScreenState extends State<ImageScreen> {
+  //storage check instance
+  StoragePermission checkStorage = StoragePermission();
   @override
+  void initState() {
+    //check the population
+    checkStoragePermission();
+    super.initState();
+  }
+
+  Future<void> checkStoragePermission() async {
+    try {
+      await Provider.of<StoragePermission>(context, listen: false)
+          .getStoragePermission();
+      if (await checkStorage.permissiongGranted) {
+        print('permission successful');
+      } else if (!await checkStorage.permissiongGranted) {
+        print('permissionfailed');
+      }
+    } catch (e) {
+      print('error::creating a permission ');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(child: Center(child: Text('Display images from path')));
   }
 }
+
+/**
+ * this is the place we shall place the images for the application 
+ * the user will be able to view the images tha are present in the location
+ *  
+ */
--- statussaver/lib/screens/navigation.dart
+++ statussaver/lib/screens/navigation.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:statussaver/screens/image.dart';
 import 'package:statussaver/screens/videos.dart';
+import 'package:statussaver/services/permission.dart';
 
 class NavigationToScreens extends StatefulWidget {
   NavigationToScreens({Key? key}) : super(key: key);
@@ -13,8 +15,8 @@ class _NavigationToScreensState extends State<NavigationToScreens> {
   int _selectedIdx = 0; //the selected screen
 
   final screens = [
-    //this is the list of screens 
-    //both to display the videos and the images 
+    //this is the list of screens
+    //both to display the videos and the images
     ImageScreen(),
     VideosFromStorage(),
   ];
@@ -28,23 +30,31 @@ class _NavigationToScreensState extends State<NavigationToScreens> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIdx,
-        onTap: _onItemTapped,
-        items: [
-          //this are the buttons to diffrent pages
-          BottomNavigationBarItem(
-            icon: Icon(Icons.image),
-            label: 'images',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.video_collection),
-            label: 'videos',
-          ),
-        ],
+    return MultiProvider(
+      providers: [
+        //raising state for the storage permission 
+        ChangeNotifierProvider.value(
+          value: StoragePermission(),
+        ),
+      ],
+      child: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIdx,
+          onTap: _onItemTapped,
+          items: [
+            //this are the buttons to diffrent pages
+            BottomNavigationBarItem(
+              icon: Icon(Icons.image),
+              label: 'images',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.video_collection),
+              label: 'videos',
+            ),
+          ],
+        ),
+        body: screens[_selectedIdx],
       ),
-      body: screens[_selectedIdx],
     );
   }
 }
