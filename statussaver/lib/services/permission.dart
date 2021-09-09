4c1041ed2ca7bc51a18ec6faea86d8b0996a7990
flavian-anselmo <anselmoflavian4@gmail.com>
2021-09-09 12:36:06 +0300

    fixed error handling in permission model

--- statussaver/lib/services/permission.dart
+++ statussaver/lib/services/permission.dart
@@ -4,17 +4,21 @@ import 'package:permission_handler/permission_handler.dart';
 class StoragePermission extends ChangeNotifier {
   bool permissiongGranted = false;
   Future getStoragePermission() async {
-    if (await Permission.storage.request().isGranted) {
-      permissiongGranted = true;
-      //notify that the value is true
-      print('permission granted');
-      notifyListeners();
+    try {
+      if (await Permission.storage.request().isGranted) {
+        permissiongGranted = true;
+        //notify that the value is true
+        print('permission granted');
+        notifyListeners();
 
-      //if the permission is denied
-    } else if (await Permission.storage.request().isDenied) {
-      permissiongGranted = false;
-      print('storage permission denied ');
-      notifyListeners();
+        //if the permission is denied
+      } else if (await Permission.storage.request().isDenied) {
+        permissiongGranted = false;
+        print('storage permission denied ');
+        notifyListeners();
+      }
+    } catch (e) {
+      print(e);
     }
   }
 }
